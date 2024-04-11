b6c62e633131e9b9877c0b3230ab09ea59c09a34
iceyisaak <me@iceyisaak.com>
2024-04-11 15:03:32 +0200

    Adjusted minor spacing issue

--- src/pages/detail-page/detail-page.tsx
+++ src/pages/detail-page/detail-page.tsx
@@ -13,18 +13,15 @@ export function DetailPage() {
             {/* <button onClick={() => navigate(-1)}>
         Back to Results
       </button> */}
-            {/* <h1 className='text-4xl'>
-                {CinemaDetail?.Title}
-            </h1> */}
 
             <div className='
-            flex flex-col
-            items-center
-            lg:flex-row  lg:justify-center
-            w-full flex-wrap
+                flex flex-col
+                items-center
+                lg:flex-row
+                lg:justify-center
+                w-full flex-wrap
             '>
-
-                <aside className='mb-20 px-5 flex items-center '>
+                <aside className='mb-20 lg:mb-0 px-5 flex items-center '>
                     <img
                         src={CinemaDetail?.Poster}
                         alt={CinemaDetail?.Title}
@@ -32,76 +29,76 @@ export function DetailPage() {
                     />
                 </aside>
                 <aside className='md:w-9/12 lg:w-6/12 px-6 py-10 '>
-                    <h1 className='text-5xl mb-5 font-extrabold'>
+                    <h1 className='text-5xl mb-6 font-extrabold'>
                         {CinemaDetail?.Title}
                     </h1>
-                    <div className='text-xl mb-1'>
+                    <div className='text-xl mb-2'>
                         <span className='font-bold text-xl'>Year:</span> {CinemaDetail?.Year}
                     </div>
-                    <div className='text-xl mb-1'>
+                    <div className='text-xl mb-2'>
                         <span className='font-bold text-xl'>Genre:</span> {CinemaDetail?.Genre}
                     </div>
-                    <div className='text-xl mb-1'>
+                    <div className='text-xl mb-2'>
                         <span className='font-bold text-xl'>Staring:</span> {CinemaDetail?.Actors}
                     </div>
-                    <div className='text-xl mb-1'>
+                    <div className='text-xl mb-2'>
                         <span className='font-bold text-xl'>Director(s):</span> {CinemaDetail?.Director}
                     </div>
-                    <div className='text-xl mb-1'>
+                    <div className='text-xl mb-2'>
                         <span className='font-bold text-xl'>Released Date:</span> {CinemaDetail?.Released}
                     </div>
                     {
                         CinemaDetail?.Website === 'N/A' ? null :
-                            <div className='text-xl mb-1'>
+                            <div className='text-xl mb-2'>
                                 <span className='font-bold text-xl'>Website:</span> {CinemaDetail?.Website}
                             </div>
                     }
-                    <div className='text-xl mb-1'>
+                    <div className='text-xl mb-2'>
                         <span className='font-bold text-xl'>Awards:</span> {CinemaDetail?.Awards}
                     </div>
-                    <div className='text-xl mb-1'>
+                    <div className='text-xl mb-2'>
                         <span className='font-bold text-xl'>Writer(s):</span> {CinemaDetail?.Writer}
                     </div>
-                    <div className='text-xl mb-1'>
+                    <div className='text-xl mb-2'>
                         <span className='font-bold text-xl'>Rated:</span> {CinemaDetail?.Rated}
                     </div>
-                    <div className='text-xl mb-1'>
+                    <div className='text-xl mb-2'>
                         <span className='font-bold text-xl'>Runtime:</span> {CinemaDetail?.Runtime}
                     </div>
 
-                    <div className='text-xl mb-1'>
+                    <div className='text-xl mb-2'>
                         <span className='font-bold text-xl'>Meta Score:</span> {CinemaDetail?.Metascore}
                     </div>
-                    <div className='text-xl mb-1'>
+                    <div className='text-xl mb-2'>
                         <span className='font-bold text-xl'>Ratings Source:</span> {CinemaDetail?.Ratings[0].Source}
                     </div>
-                    <div className='text-xl mb-1'>
+                    <div className='text-xl mb-2'>
                         <span className='font-bold text-xl'>Ratings Earned:</span> {CinemaDetail?.Ratings[0].Value}
                     </div>
                     {
                         CinemaDetail?.Production === 'N/A' ?
                             null :
-                            <div className='text-xl mb-1'>
+                            <div className='text-xl mb-2'>
                                 <span className='font-bold text-xl'>Production</span>{CinemaDetail?.Production}
                             </div>
                     }
-                    <div className='text-xl mb-1'>
+                    <div className='text-xl mb-2'>
                         <span className='font-bold text-xl'>Box Office:</span> {CinemaDetail?.BoxOffice}
                     </div>
-                    <div className='text-xl mb-1'>
+                    <div className='text-xl mb-2'>
                         <span className='font-bold text-xl'>Language(s):</span> {CinemaDetail?.Language}
                     </div>
                     <div className='font-bold text-xl'>Summary: </div>
-                    <div className='text-xl mb-1'>
+                    <div className='text-xl mb-2'>
                         {CinemaDetail?.Plot}
                     </div>
-                    <div className='text-xl mb-1'>
+                    <div className='text-xl mb-2'>
                         <span className='font-bold text-xl'>IMDb ID: </span> {CinemaDetail?.imdbID}
                     </div>
-                    <div className='text-xl mb-1'>
+                    <div className='text-xl mb-2'>
                         <span className='font-bold text-xl'>IMDb Rating: </span>{CinemaDetail?.imdbRating}
                     </div>
-                    <div className='text-xl mb-1'>
+                    <div className='text-xl mb-2'>
                         <span className='font-bold text-xl'>IMDb Vote: </span> {CinemaDetail?.imdbVotes}
                     </div>
                 </aside>
