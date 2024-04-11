882eff1d6c9823138a39afd396730480be0f49cb
iceyisaak <me@iceyisaak.com>
2024-04-11 15:27:54 +0200

    Adjusted width in Home

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -5,7 +5,7 @@ export function Home() {
     return (
         <section>
             <article className="flex w-screen h-screen bg-gray-950 justify-center pt-60">
-                <div className="h-max w-6/12 flex flex-col items-center">
+                <div className="h-max w-11/12 md:w-10/12 lg:w-6/12 flex flex-col items-center">
                     <h1 className="text-8xl mb-5 text-yellow-300 flex">
                         Movie Lookup<RiMovie2Line />
                     </h1>
