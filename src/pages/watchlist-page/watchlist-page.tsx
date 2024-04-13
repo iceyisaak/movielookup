127f471cb47ca930c43aded2bd01ae1ec57190ff
iceyisaak <me@iceyisaak.com>
2024-04-13 16:48:07 +0200

    Added basic styling for WatchlistPage

--- src/pages/watchlist-page/watchlist-page.tsx
+++ src/pages/watchlist-page/watchlist-page.tsx
@@ -1,6 +1,17 @@
 
 export function WatchlistPage() {
     return (
-        <div>WatchlistPage</div>
+        <section className="w-screen h-screen pt-32 pb-52">
+            <article className="mb-5 px-5">
+                <h2 className="text-4xl text-gray-300 mb-1">
+                    Watchlist
+                </h2>
+            </article>
+            <article className="pt-10">
+                <h3 className="text-2xl text-center">
+                    Your Watchlist is Empty :/
+                </h3>
+            </article>
+        </section>
     )
 }
