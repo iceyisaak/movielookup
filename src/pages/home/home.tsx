e27c459a9bb5b63433e04250cedc35143a477996
iceyisaak <me@iceyisaak.com>
2026-03-11 17:08:08 +0100

    Added link to watchlist in home.tsx

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,18 +1,31 @@
-import { RiMovie2Line } from "react-icons/ri";
 import { SearchForm } from "../../components/search-form";
+import { useWatchlistStore } from "../../features/store/watchlist-store";
+import { RiMovie2Line, RiPlayList2Fill } from "react-icons/ri";
+import { Link } from "react-router-dom";
 
 export function Home() {
-    return (
-        <section>
-            <article className="flex w-screen h-screen bg-gray-950 justify-center pt-60">
-                <div className="h-max w-11/12 md:w-10/12 lg:w-6/12 flex flex-col items-center">
-                    <h1 className="text-8xl mb-5 text-yellow-300 flex">
-                        Movie Lookup<RiMovie2Line />
-                    </h1>
-                    <RiMovie2Line />
-                    <SearchForm />
-                </div>
-            </article>
-        </section>
-    )
+  const ids = useWatchlistStore((s) => s.ids);
+  const hasWatchlist = ids.length > 0;
+  return (
+    <section>
+      <article className="flex w-screen h-screen bg-gray-950 justify-center pt-60">
+        <div className="h-max w-11/12 md:w-10/12 lg:w-6/12 flex flex-col items-center">
+          <h1 className="text-8xl mb-5 text-yellow-300 flex">
+            Movie Lookup
+            <RiMovie2Line />
+          </h1>
+          <RiMovie2Line />
+          <SearchForm />
+        </div>
+        {hasWatchlist && (
+          <Link
+            to="/watchlist"
+            className="absolute top-6 right-6 flex items-center gap-2"
+          >
+            <RiPlayList2Fill size={30} className="text-gray-600" />
+          </Link>
+        )}
+      </article>
+    </section>
+  );
 }
